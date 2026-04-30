56a93c26300d914e3818c7eb0cd0ce651ede4cec
Zachary Johnston <ZacharyJohnston324@u.boisestate.edu>
2026-04-29 22:06:36 -0600

    Fix Schema (I can't count)

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -1,7 +1,7 @@
 CREATE table Class (
     ID int not null auto_increment ,
     course_num varchar(10),
-    term varchar(5),
+    term varchar(6),
     section_num varchar(3),
     description text,
     PRIMARY KEY (`ID`)
