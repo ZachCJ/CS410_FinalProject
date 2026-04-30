5305528d17e818ccf20af9a079f48220ba052366
Zachary Johnston <ZacharyJohnston324@u.boisestate.edu>
2026-04-29 22:37:22 -0600

    dump

---
+++ SQL/dump.sql
@@ -0,0 +1,149 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: final
+-- ------------------------------------------------------
+-- Server version	9.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Assigned`
+--
+
+DROP TABLE IF EXISTS `Assigned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Assigned` (
+  `Student_ID` int NOT NULL,
+  `Assignment_ID` int NOT NULL,
+  `grade` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`Student_ID`,`Assignment_ID`),
+  KEY `Assigned_ibfk_1` (`Assignment_ID`),
+  CONSTRAINT `Assigned_ibfk_1` FOREIGN KEY (`Assignment_ID`) REFERENCES `Assignment` (`ID`),
+  CONSTRAINT `Assigned_ibfk_2` FOREIGN KEY (`Student_ID`) REFERENCES `Student` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Assignment`
+--
+
+DROP TABLE IF EXISTS `Assignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Assignment` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `description` text,
+  `point_value` decimal(10,0) DEFAULT NULL,
+  `Category_ID` int NOT NULL,
+  `Class_ID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `name` (`name`,`Class_ID`),
+  KEY `Assignment_ibfk_1` (`Category_ID`),
+  KEY `Assignment_ibfk_2` (`Class_ID`),
+  CONSTRAINT `Assignment_ibfk_1` FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`),
+  CONSTRAINT `Assignment_ibfk_2` FOREIGN KEY (`Class_ID`) REFERENCES `Class` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Category`
+--
+
+DROP TABLE IF EXISTS `Category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Category` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Class`
+--
+
+DROP TABLE IF EXISTS `Class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Class` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `course_num` varchar(10) DEFAULT NULL,
+  `term` varchar(6) DEFAULT NULL,
+  `section_num` varchar(3) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ClassHasCategory`
+--
+
+DROP TABLE IF EXISTS `ClassHasCategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ClassHasCategory` (
+  `Class_ID` int NOT NULL,
+  `Category_ID` int NOT NULL,
+  `weight` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`Class_ID`,`Category_ID`),
+  KEY `ClassHasCategory_ibfk_2` (`Category_ID`),
+  CONSTRAINT `ClassHasCategory_ibfk_1` FOREIGN KEY (`Class_ID`) REFERENCES `Class` (`ID`),
+  CONSTRAINT `ClassHasCategory_ibfk_2` FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Enrolled`
+--
+
+DROP TABLE IF EXISTS `Enrolled`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Enrolled` (
+  `Class_ID` int NOT NULL,
+  `Student_ID` int NOT NULL,
+  PRIMARY KEY (`Class_ID`,`Student_ID`),
+  KEY `Enrolled_ibfk_2` (`Student_ID`),
+  CONSTRAINT `Enrolled_ibfk_1` FOREIGN KEY (`Class_ID`) REFERENCES `Class` (`ID`),
+  CONSTRAINT `Enrolled_ibfk_2` FOREIGN KEY (`Student_ID`) REFERENCES `Student` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Student`
+--
+
+DROP TABLE IF EXISTS `Student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Student` (
+  `ID` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `username` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-29 22:32:52
